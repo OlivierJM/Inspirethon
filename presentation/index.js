de1de631bc245de20f121e69b8050633d59e8554
Olivier JM <manolivier93@gmail.com>
2018-11-09 21:54:02 +0200

    finished a basic presentation

--- presentation/index.js
+++ presentation/index.js
@@ -11,11 +11,15 @@ import {
   List,
   Quote,
   Slide,
-  Text
+  Text,
+  Image,
+  GoToAction,
+  CodePane
 } from "spectacle";
 
 // Import theme
 import createTheme from "spectacle/lib/themes/default";
+import myImage from '../assets/olivier.jpg'
 
 // Require CSS
 require("normalize.css");
@@ -35,36 +39,103 @@ export default class Presentation extends React.Component {
     return (
       <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
         <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps={false} lineHeight={1} textColor="secondary">
-            Olivier JM
+    
+          <Heading size={1} fit caps={false} lineHeight={1} textColor="tertiary">
+          "Programming" Your Next Dream Job 
           </Heading>
           <br />
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            "Programming" Your Next Dream Job 
+          <br />
+          <br />
+          <br />
+          <Text>
+            @olivierjm on Github, Twitter and Medium
           </Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
+        <Slide transition={["zoom"]} bgImage={myImage}>
+          <br />
+          <Text margin="10px 0 0" textColor="tertiary" size={6} fit bold>
+              My little Story 
+          </Text>
+        <Heading size={5} textColor="white">Work Hard until ...</Heading>
         </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
+        <Slide transition={['fade']} >
+        <Text margin="10px 0 0" textColor="tertiary" size={6} fit bold>
+              Let's Consider the current Status 
+        </Text>
+        <List>
+            <ListItem>Education</ListItem>
+            <ListItem>Health</ListItem>
+            <ListItem>Finance</ListItem>
+            <ListItem>The General Community</ListItem>
           </List>
+        </Slide>
+        <Slide transition={['fade']} >
+          <Text margin="10px 0 0" textColor="tertiary" size={6} fit bold>
+            Where is the issue here 🤔 🤔 🤔???
+          </Text>
+          <br />
+          <br />
+          <br />
+          <br />
+
+          Education is the key, but ...😒 
+        </Slide>
+        <Slide>
+          <Text margin="10px 0 0" textColor="tertiary" size={6} fit bold>
+            What is Programming ?  
+          </Text>
+          <br />
+          <br />
+         
+          <Text>
+            💻 🖥  👩‍💻 👨‍💻
+          </Text>
+        </Slide>
+        <Slide>
+          <Text margin="10px 0 0" textColor="tertiary" size={6} fit bold>
+            Why learn Programming ?  
+          </Text>
+          <CodePane
+            lang="js"
+            source={require('raw-loader!../assets/deck.example')}
+            margin="20px auto"
+            // overflow="overflow"
+          />
+
+        </Slide>
+
+         <Slide transition={["fade"]} bgColor="primary">
+          {/* <Text size={6} textColor="secondary" caps>Going Back 👈 👈</Text> */}
+          <GoToAction slide={3}> Consider this again 👈 👈</GoToAction>
+        </Slide>
+   
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <BlockQuote>
+              Whether you want to uncover the secrets of the universe, or you just want to pursue 
+              a career in the 21stcentury, basic computer programming 
+              is an essential skill to learn.
+            <Cite>Stephen Hawking</Cite>
+          </BlockQuote>
         </Slide>
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
           <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
+            Everybody should learn to program a computer, because it teaches you how to think
+            <Cite>Steve Jobs</Cite>
+          </BlockQuote>
+        </Slide>
+
+
+        <Slide>
+          <Text margin="10px 0 0" textColor="tertiary" size={6} fit bold>
+            What you and I can and Should do <br /><br />
+             💪💪💪💪
+          </Text>
+        </Slide>
+  
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <BlockQuote>
+            <Quote>Our future lies in the code we write</Quote>
+            <Cite>OlivierJM 😉</Cite>
           </BlockQuote>
         </Slide>
       </Deck>
